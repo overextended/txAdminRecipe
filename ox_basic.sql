d676d2edf5cfe17263634507a62f4e65995dfc98
DokaDoka <maxwelljamesmurphy@gmail.com>
2021-12-26 17:39:16 +1100

    fix(sql): add missing queries

--- ox_basic.sql
+++ ox_basic.sql
@@ -43,3 +43,35 @@ CREATE TABLE `jobs` (
 );
 
 INSERT INTO `jobs` VALUES ('unemployed','Unemployed');
+
+ALTER TABLE `users`
+	ADD COLUMN `firstname` VARCHAR(16) NULL DEFAULT NULL,
+	ADD COLUMN `lastname` VARCHAR(16) NULL DEFAULT NULL,
+	ADD COLUMN `dateofbirth` VARCHAR(10) NULL DEFAULT NULL,
+	ADD COLUMN `sex` VARCHAR(1) NULL DEFAULT NULL,
+	ADD COLUMN `height` INT NULL DEFAULT NULL
+;
+
+CREATE TABLE `ox_inventory` (
+	`owner` varchar(60) DEFAULT NULL,
+	`name` varchar(100) NOT NULL,
+	`data` longtext DEFAULT NULL,
+	`lastupdated` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+	UNIQUE KEY `owner` (`owner`,`name`)
+);
+
+-- Setup new columns for vehicle stashes, and actually index owner
+ALTER TABLE `owned_vehicles`
+	ADD COLUMN `trunk` LONGTEXT NULL,
+	ADD COLUMN `glovebox` LONGTEXT NULL,
+	ADD INDEX `owner` (`owner`)
+;
+
+ALTER TABLE `user_licenses`
+	ADD INDEX `owner` (`owner`)
+;
+
+-- Now I'm just being too kind
+ALTER TABLE `vehicles`
+	ADD INDEX `category` (`category`
+);
